ecdfeb3000a88953272dc3ae62d6edca1c999e6c
JS Caughlin <jcaughlin@madisoncollege.edu>
2018-03-09 09:00:48 -0600

    worked on styling margins

--- src/app/pizza-maker/pizza-maker.component.ts
+++ src/app/pizza-maker/pizza-maker.component.ts
@@ -17,12 +17,35 @@ export class PizzaMakerComponent implements OnInit {
   constructor(private pizzaToppings: PizzaMakerService) { }
   
   public pizzaMaker: pizzaToppings[];
+  public pieOrder = '';
+  public orderNumber = '';
 
   ngOnInit() {
-    this.pizzaMaker = this.pizzaToppings.getAvailablePizzaToppings()
-  	.map(x => ({name: x, checked: false}));
+    this.unCheckAll();
   }
 
-}
+  checkAll(){
+    this.pizzaMaker = this.pizzaToppings.getAvailablePizzaToppings()
+      .map(x => ({ name: x, checked: true }));
+  }
+
+  unCheckAll(){
+    this.pizzaMaker = this.pizzaToppings.getAvailablePizzaToppings()
+      .map(x => ({ name: x, checked: false }));
+  }
+
+
+  orderPie() {
+    let selected = this.pizzaMaker.filter(x => x.checked == true);
+    if (selected.length > 0) {
+      this.pieOrder = "You ordered: ";
+      selected.forEach(x => this.pieOrder += x.name + ', ');
+      this.pieOrder = this.pieOrder.slice(0, -2) + "!";
+    } else {
+      this.pieOrder = '';
+    }
+  }
+  }
+
 
   
